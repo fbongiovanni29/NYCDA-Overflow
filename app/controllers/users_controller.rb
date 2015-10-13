add7dc783622ecfd37b586ef158a2d52ccb4ad45
Julio Rodriguez <lemaisonmusic@gmail.com>
2015-10-13 15:37:55 -0400

    Users controller updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,27 @@ class UsersController < ApplicationController
   end
 
   def new
+  	@user = User.new
   end
+
+  def create
+  	@user = User.create(user_params)
+  	redirect_to @user
+  end 
 
   def show
   end
 
   def edit
   end
+
+  private 
+
+  	def user_params 
+	  	params.require(:user).permit(
+	  		:firstname,
+	  		:lastname,
+	  		:email,
+	  		:password_digest)
+  	end 
 end
