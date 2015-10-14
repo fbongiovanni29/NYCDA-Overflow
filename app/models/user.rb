305a2d9c26099ccf6d012a0c6beefdd5c05adf03
Francis Bongiovanni <fbongiovanni29@gmail.com>
2015-10-14 16:31:46 -0400

    Signup functioning

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,13 @@
-require 'bcrypt'
-
 class User < ActiveRecord::Base
-  include BCrypt
-  attr_accessor :password
+  # attr_accessor :password
 
-  def password
-	  @password ||= Password.new(password_digest)
-  end
-
-  def password=(new_password)
-	  @password = Password.create(new_password)
-	  self.password_digest = @password
-  end
+    has_secure_password
   
-  has_secure_password
+
   validates_presence_of :firstname
   validates_presence_of :lastname
-  validates :email, :presence => true, :uniqueness => true
-  #validates :password, length: { in: 6..20 }
-  
+  validates :password, length: { in: 6..20, message: "Length needs to be between 6 and 20 characters" }
 
+  validates :email, :presence => true, :uniqueness => true
+  
 end
