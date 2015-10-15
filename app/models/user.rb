0fd45b5ca88e53a5c88483580b54e7d4a4bc25f9
Francis Bongiovanni <fbongiovanni29@gmail.com>
2015-10-15 11:54:32 -0400

    Edit User pushes to db need to fix bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,12 @@ class User < ActiveRecord::Base
 
     has_secure_password
   
+#validates presence of user paramaters on the create controller
+  validates_presence_of :firstname, :on => :create
+  validates_presence_of :lastname, :on => :create
+  validates :password, length: { in: 6..20, message: "Length needs to be between 6 and 20 characters" }, allow_nil: true, :on => :create
 
-  validates_presence_of :firstname
-  validates_presence_of :lastname
-  validates :password, length: { in: 6..20, message: "Length needs to be between 6 and 20 characters" }
-
-  validates :email, :presence => true, :uniqueness => true
+  validates :email, :presence=> true, :uniqueness => true, :on => :create
 
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
