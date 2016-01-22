46044f620eb79a29f0ae655c5fdb634c28fff392
Francis Bongiovanni <fbongiovanni29@gmail.com>
2016-01-22 13:44:40 -0500

    Refactored routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,25 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :show, :new, :create, :edit] do
-  	resources :posts, only: [:index, :new, :show]
-  end
 
-  resources :answers, only: [] do
-	  member do
-		  put "like", to: "answers#upvote"
-		  put "dislike", to: "answers#downvote"
-	  end
-  end
-
-  post '/users/:user_id/posts/:post_id' => 'answers#create'
+  #roots to the logged in home page
   root 'posts#home_page'
-  get '/signup' => 'users#new'
-  post '/signup' => 'users#create'
-  post '/login', to: 'sessions#create', as: :login
-  delete '/logout', to: 'sessions#destroy', as: :logout
-  put '/users/:user_id/posts/:post_id' => 'posts#upvote'
 
+  #creates the answer
+  post '/users/:user_id/posts/:post_id' => 'answers#create'
+
+  #sessions login/out
+  resources :sessions, only: [:create, :destroy]
+  
+  #routes for users
+  resources :users, only: [:index, :show, :new, :create, :edit] do
+    #nested routes for posts
+    resources :posts, only: [:index, :new, :show, :create]
+  end
+
+  #nests the votes
+  resources :answers, only: [] do
+    member do
+      put "like", to: "answers#upvote"
+      put "dislike", to: "answers#downvote"
+    end
+  end
 end
