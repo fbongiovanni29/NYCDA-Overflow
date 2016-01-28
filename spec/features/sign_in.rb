c06a8de6a7434749a759c1d6bc7e625f574e0be8
Francis Bongiovanni <fbongiovanni29@gmail.com>
2016-01-28 11:40:15 -0500

    unsuccesful signin

--- spec/features/sign_in.rb
+++ spec/features/sign_in.rb
@@ -1,19 +1,35 @@
 require 'rails_helper'
 
-RSpec.feature "Signing in" do
+feature "Successul sign in" do
   background do
-    FactoryGirl.create(:user, email: "fbongiovanni29@gmail.com", password: "password", firstname: "francis", lastname: "Bongiovanni")
+   create(:user, email: "fbongiovanni29@gmail.com", password: "password", firstname: "francis", lastname: "Bongiovanni")
   end
 
   scenario "Signing in with correct credentials" do
     visit new_user_path 
-    expect(current_path).to eq('/users/new')
       within("#signin") do
 	fill_in 'email', :with => 'fbongiovanni29@gmail.com' 
 	fill_in 'password', :with => 'password'
       end
       click_button 'Login'
       expect(current_path).to eq('/')
-      expect(page).to have_css('ul.nav')
+      expect(page).to have_link('Logout') 
+  end
+end
+
+feature "Unsuccessul sign in" do
+  background do
+   create(:user, email: "fbongiovanni29@gmail.com", password: "password", firstname: "francis", lastname: "Bongiovanni")
+  end
+
+  scenario "Signing in with correct credentials" do
+    visit new_user_path 
+      within("#signin") do
+	fill_in 'email', :with => 'fbongiovanni29@gmail.com' 
+	fill_in 'password', :with => 'notpassword'
+      end
+      click_button 'Login'
+      expect(current_path).to eq('/users/new')
+      expect(page).to have_css('form#signin')
   end
 end
