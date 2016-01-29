5158fa48a26d848b08107366bdd5b89420e3e445
Francis Bongiovanni <fbongiovanni29@gmail.com>
2016-01-28 19:29:45 -0500

    all sign in tests working

--- spec/features/sign_in.rb
+++ spec/features/sign_in.rb
@@ -1,5 +1,92 @@
 require 'rails_helper'
 
+feature "User types wrong password" do
+  background do
+   create(:user, email: "fbongiovanni29@gmail.com", password: "password", firstname: "francis", lastname: "Bongiovanni")
+  end
+
+  scenario "Signing in with correct credentials" do
+    visit new_user_path 
+      within("#signin") do
+	fill_in 'email', :with => 'fbongiovanni29@gmail.com' 
+	fill_in 'password', :with => 'notpassword'
+      end
+      click_button 'Login'
+      expect(current_path).to eq('/users/new')
+      expect(page).to have_css('form#signin')
+  end
+end
+
+
+feature "User types empty password" do
+  background do
+   create(:user, email: "fbongiovanni29@gmail.com", password: "password", firstname: "francis", lastname: "Bongiovanni")
+  end
+
+  scenario "Sign in with empty password" do
+    visit new_user_path 
+      within("#signin") do
+	fill_in 'email', :with => 'fbongiovanni29@gmail.com' 
+	fill_in 'password', :with => ''
+      end
+      click_button 'Login'
+      expect(current_path).to eq('/users/new')
+      expect(page).to have_css('form#signin')
+  end
+end
+
+feature "User types wrong username" do
+  background do
+   create(:user, email: "fbongiovanni29@gmail.com", password: "password", firstname: "francis", lastname: "Bongiovanni")
+  end
+
+  scenario "Signing in with correct credentials" do
+    visit new_user_path 
+      within("#signin") do
+	fill_in 'email', :with => 'wrong@gmail.com' 
+	fill_in 'password', :with => 'notpassword'
+      end
+      click_button 'Login'
+      expect(current_path).to eq('/users/new')
+      expect(page).to have_css('form#signin')
+  end
+end
+
+feature "User types empty username" do
+  background do
+   create(:user, email: "fbongiovanni29@gmail.com", password: "password", firstname: "francis", lastname: "Bongiovanni")
+  end
+
+  scenario "Signing in with correct credentials" do
+    visit new_user_path 
+      within("#signin") do
+	fill_in 'email', :with => '' 
+	fill_in 'password', :with => 'notpassword'
+      end
+      click_button 'Login'
+      expect(current_path).to eq('/users/new')
+      expect(page).to have_css('form#signin')
+  end
+end
+
+
+feature "User types empty username & password" do
+  background do
+   create(:user, email: "fbongiovanni29@gmail.com", password: "password", firstname: "francis", lastname: "Bongiovanni")
+  end
+
+  scenario "Signing in with correct credentials" do
+    visit new_user_path 
+      within("#signin") do
+	fill_in 'email', :with => '' 
+	fill_in 'password', :with => ''
+      end
+      click_button 'Login'
+      expect(current_path).to eq('/users/new')
+      expect(page).to have_css('form#signin')
+  end
+end
+
 feature "Successul sign in" do
   background do
    create(:user, email: "fbongiovanni29@gmail.com", password: "password", firstname: "francis", lastname: "Bongiovanni")
@@ -17,19 +104,3 @@ feature "Successul sign in" do
   end
 end
 
-feature "Unsuccessul sign in" do
-  background do
-   create(:user, email: "fbongiovanni29@gmail.com", password: "password", firstname: "francis", lastname: "Bongiovanni")
-  end
-
-  scenario "Signing in with correct credentials" do
-    visit new_user_path 
-      within("#signin") do
-	fill_in 'email', :with => 'fbongiovanni29@gmail.com' 
-	fill_in 'password', :with => 'notpassword'
-      end
-      click_button 'Login'
-      expect(current_path).to eq('/users/new')
-      expect(page).to have_css('form#signin')
-  end
-end
